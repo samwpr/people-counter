5a40c2e635a0abb04c2817f1ef0325a4b6e1744f
samwpr <swpr10x@gmail.com>
2022-09-14 15:49:54 +0800

    A simple people counter

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-// document.getElementById("count-el").innerHTML = 5;
-let count = 0 
-let myAge = 28
-console.log(myAge)
+
+document.getElementById("container").style.backgroundImage = "url('img_test.png')";
+
+var count = 0;
+
+document.getElementById("increment-btn").onclick = function increment(){
+    count += 1;
+    document.getElementById("count-el").innerHTML = count;
+};
+
+document.getElementById("decrement-btn").onclick = function decrement(){
+    count -= 1;
+    document.getElementById("count-el").innerHTML = count;
+};
+
+document.getElementById("reset-btn").onclick = function reset(){
+    count = 0;
+    document.getElementById("count-el").innerHTML = count;
+};
